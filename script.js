f6c57e5c094b2851f7ff1b35c971c5a83f348f4a
zakibit <your-github-zathan1162@gmail.com>
2026-08-10 07:44:30 +0100

    upated menu.html

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 const menuButton = document.querySelector(".hamBurgerMenu");
 const navList = document.querySelector(".navList");
 const navLinks = document.querySelectorAll(".nav");
-console.log(menuButton);
+const categoryButtons = document.querySelectorAll(".category");
+const productCards = document.querySelectorAll(".productCard");
+const searchBox = document.querySelector("#searchBox");
+
 menuButton.addEventListener("click", function(){
     navList.classList.toggle("menuOpen");
     menuButton.textContent = navList.classList.contains("menuOpen") ? "✕" : "☰";
@@ -10,8 +13,57 @@ navLinks.forEach(function(link){
 
     link.addEventListener("click", function(){
 
-        navList.classList.remove("openMenu");
+        navList.classList.remove("menuOpen");
         menuButton.textContent = "☰";
+
+    });
+
+});
+document.addEventListener("click", function (event) {
+    if (!event.target.closest(".navBar")) {navList.classList.remove("menuOpen");
+        menuButton.textContent = "☰";
+    }
+});
+
+categoryButtons.forEach(function(button){
+
+    button.addEventListener("click", function(){
+
+        categoryButtons.forEach(function(category){
+            category.classList.remove("activeCategory");
+        });
+
+        button.classList.add("activeCategory");
+
+        const selectedCategory = button.dataset.category;
+
+        productCards.forEach(function(card){
+
+            const productCategory = card.dataset.category;
+
+            if(selectedCategory === "all" || selectedCategory === productCategory){
+                card.classList.remove("hidden");
+            } else {
+                card.classList.add("hidden");
+            }
+
+        });
+
+    });
+
+});
+
+searchBox.addEventListener("input", function(){
+    const searchTerm = searchBox.value.toLowerCase();
+    productCards.forEach(function(card){
+
+        const productName = card.querySelector("h3").textContent.toLowerCase();
+
+        if(productName.includes(searchTerm)){
+            card.classList.remove("hidden");
+        } else {
+            card.classList.add("hidden");
+        }
 
     });
 
